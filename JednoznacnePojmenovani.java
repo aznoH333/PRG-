6e07303491c501b5db1ad4e235464e653fef0d9f
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2021-09-16 16:50:06 +0200

    ano

---
+++ JednoznacnePojmenovani.java
@@ -0,0 +1,9 @@
+public class JednoznacnePojmenovani {
+    public static void main(String[] args) {
+        double dylka = 6;
+        double vyska = 6;
+        double velykost = 4;
+
+        System.out.println(Math.ceil(dylka/velykost) * Math.ceil(vyska/velykost));
+    }
+}
